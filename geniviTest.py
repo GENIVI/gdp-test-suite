4c0a4e0ecbea9ba559683fb9db7b8af8c8659993
Robert Marshall <robert.marshall@codethink.co.uk>
2016-08-26 08:19:34 +0100

    Make testing more modular
    Added a sendCommand method so test creators don't have to worry about the ssh command
    Use setUpClass and tearDownClass so I can use class variables more easily

--- geniviTest.py
+++ geniviTest.py
@@ -3,6 +3,12 @@ import unittest
 from subprocess import call, Popen, check_output
 import time
 
+#
+# Assumptions
+# Script is run in gdp-src-build
+# Python modules unittest, time
+#
+
 # variables which might need changing
 arch='qemux86-64'
 dir='tmp/deploy/images/'+arch+'/'
@@ -21,7 +27,7 @@ kvmCmd = [
           '-net', 'user,hostfwd=tcp::'+port+'-:22', # open port 5555 for ssh access
           '-cpu', 'core2duo',
           '-hda', dir+fs, 
-          '-vga', 'vmware',  '-no-reboot', '-m', '512',
+          '-vga', 'std',  '-no-reboot', '-m', '512',
           '--append', 'vga=0 uvesafb.mode_option=640x480-32 root=/dev/hda rw mem=512M  oprofile.timer=1 -serial stdio'
           ]
 
@@ -42,43 +48,49 @@ class TestGeniviQemu(unittest.TestCase):
         if (self.kvm != None):
             call(baseSsh + ["poweroff"])
 
+    def sendCommand(self,cmd):
+        op = check_output(baseSsh +cmd)
+        return op
+    
     def test_checkErrors(self):
         # tests for errors on startup, searching the output of dmesg for occurrences of the word error
-        op = check_output(baseSsh + ['dmesg',' |', 'grep', 'error', '|', 'wc', '-l'] )
+        op = self.sendCommand(['dmesg',' |', 'grep', 'error', '|', 'wc', '-l'] )
         self.assertEqual(int(op),0)
 
     def test_checkFails(self):
-        # tests for errors on startup, searching the output of dmesg for occurrences of the word error
-        op = check_output(baseSsh + ['dmesg',' |', 'grep', '[Ff]ailed', '|', 'wc', '-l'] )
+        # tests for errors on startup, searching the output of dmesg for occurrences of the word failed
+        op = self.sendCommand(['dmesg',' |', 'grep', '[Ff]ailed', '|', 'wc', '-l'] )
+        # just the one failure:
         # acpi PNP0A03:00: _OSC failed (AE_NOT_FOUND); disabling ASPM
         #print '<', op, '>'
         self.assertEqual(int(op),1)
 
     def test_checkQemu(self):
         # looks for a qemux architecture in the dmesg output
-        op = check_output(baseSsh + ['dmesg', '-t', '|', 'grep', 'qemux'])
+        op = self.sendCommand(['dmesg', '-t', '|', 'grep', 'qemux'])
         self.assertEqual(op[0:-1],'systemd[1]: Set hostname to <'+arch+'>.') # trim EOL
-        #op = check_output(baseSsh + ['dmesg',' |', 'grep', 'qemux' , ' |',  'awk {print $NF}' ], shell=True)
-        #self.assertEqual(op,'<qemux86-64>.') #'Set hostname to <qemux86>.') # trim prefix
 
     def test_checkSystemCtl(self):
         # check weston is running
-        op = check_output(baseSsh + ['systemctl', 'is-active', 'weston'])
+        op = self.sendCommand(['systemctl', 'is-active', 'weston'])
+        print 'got ', op
         # assumes Linux style EOLs
         self.assertEqual(op, 'active\n')
 
     # this test seems to run last so no need for a restart??
     # and a test to timeout because it is shutdown?
+    # A failure is expected because the image should be shutdown
     @unittest.expectedFailure
     def test_restart(self):
-        call(baseSsh + ["poweroff"])
+        self.sendCommand(["poweroff"])
         #time.sleep(2)
         self.kvm = None
-        op = check_output(baseSsh, "uptime")
+        op = self.sendCommand("uptime")
         self.assertEqual(op,"") # should have error'ed on the previous line
         self.pid = Popen(kvmCmd).pid
         
     def untest_checkSystemCtlActive(self):
+        # This test is not run
         # checks the number of active system services (is this too prescriptive?
         op = check_output(baseSsh + ['systemctl', '|', 'grep', 'active', '|', 'grep', 'inactive']) #, '|', 'grep', '362'])
         #print "<", op, ">" hmm is it really const?
