37248f218c3e4043e7b0c6f4be0bfc9abc40aadf
Robert Marshall <robert.marshall@codethink.co.uk>
2016-09-14 12:55:20 +0100

    Moved core tests into separate module
    Created coreTests.py runing geneiviTests.py will now only check the image is there.
    
    Added comments to testRunner.py to assist test writers

--- geniviTest.py
+++ geniviTest.py
@@ -34,7 +34,7 @@ else:
     dir='tmp/deploy/images/'+arch+'/'
 if not os.path.isfile(dir+image) :
     print fs
-    raise Exception("Image file not found")
+    raise Exception("Image file not found - do you need to set QEMU_IMAGE_DIR?")
 
 # Assumes that the image has been built with EXTRA_USERS_PARAMS = ""
 # If it hasn't you may need to install sshpass and edit the parameters!
@@ -55,6 +55,8 @@ class TestGeniviQemu(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.kvm = Popen(kvmCmd)
+        self.arch=arch
+        self.port = port
         # pid = kvm.pid
         #print kvm.returncode
         if (self.kvm.returncode != None):
@@ -84,63 +86,6 @@ class TestGeniviQemu(unittest.TestCase):
         op = check_output(baseSsh +cmd)
         return op
 
-    # The following are sample tests
-    def test_checkErrors(self):
-        # tests for errors on startup, searching the output of dmesg for occurrences of the word error
-        op = self.sendCommand(['dmesg',' |', 'grep', 'error', '|', 'wc', '-l'] )
-        self.assertEqual(int(op),0)
-
-    def test_checkFails(self):
-        # tests for errors on startup, searching the output of dmesg for occurrences of the word failed
-        op = self.sendCommand(['dmesg',' |', 'grep', '[Ff]ailed', '|', 'wc', '-l'] )
-        # just the one failure:
-        # acpi PNP0A03:00: _OSC failed (AE_NOT_FOUND); disabling ASPM
-        #print '<', op, '>'
-        self.assertEqual(int(op),1)
-
-    def test_checkQemu(self):
-        # looks for a qemux architecture in the dmesg output
-        op = self.sendCommand(['dmesg', '-t', '|', 'grep', 'qemux'])
-        self.assertEqual(op[0:-1],'systemd[1]: Set hostname to <'+arch+'>.') # trim EOL
-
-
-    def test_checkPythonInstall(self):
-        # op = self.sendCommand(['qml-example'])
-        # qml-example can't yet fake a return instead copy a subset of these tests
-        # onto the image and run then on the image
-        # locally
-        # Not sure what this checks apart from the scp & the image having a working python installation!
-        # needs a path prefix to py2ex.py
-        scriptPath =  os.path.dirname(os.path.realpath(sys.argv[0]))
-        call(['scp', '-o', 'StrictHostKeyChecking=no', '-P', port, scriptPath + '/py2ex.py', 'root@127.0.0.1:/tmp'])
-        op = self.sendCommand(['/tmp/py2ex.py'])
-        return True
-    
-    def test_checkSystemCtl(self):
-        # check weston is running
-        op = self.sendCommand(['systemctl', 'is-active', 'weston'])
-        # assumes Linux style EOLs
-        self.assertEqual(op, 'active\n')
-
-    # this test seems to run last so no need for a restart??
-    # and a test to timeout because it is shutdown?
-    # A failure is expected because the image should be shutdown
-    @unittest.expectedFailure
-    def test_restart(self):
-        self.sendCommand(["poweroff"])
-        #time.sleep(2)
-        self.kvm = None
-        op = self.sendCommand("uptime")
-        self.assertEqual(op,"") # should have error'ed on the previous line
-        self.pid = Popen(kvmCmd).pid
-        
-    def untest_checkSystemCtlActive(self):
-        # This test is not run
-        # checks the number of active system services (is this too prescriptive?
-        op = check_output(baseSsh + ['systemctl', '|', 'grep', 'active', '|', 'grep', 'inactive']) #, '|', 'grep', '362'])
-        #print "<", op, ">" hmm is it really const?
-        self.assertEqual(int(op.split(None, 1)[0]), 362)
-        # '362 loaded units listed. Pass --all to see loaded but inactive units, too.')
 
     
 
--- testRunner.py
+++ testRunner.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python
 
+# How to describe the tests/behavoiurs:
+# * Define an scenario (one or two sentences)
+# * Describe the acceptance test from the business and target perspective using the terminology: Given (set up) -> When (trigger) -> Then (verify) 
+# * Description of the test as comments: Initial system state -> event (or action) -> Final system state & output -> Expected state & output 
+
+# Scenario
+# The user has shell access on image as root. Wayland is up and running and we want to ensure weston is also up and running.
+
+# Acceptance test
+# Test to confirm that  Weston is up and running
+# Given that wayland was up and running
+# And yy was up and runing
+# When getting into the shell as root 
+# And ask the system about weston
+# Then sss is seen on the shell
+
+# Technical description
+
 import geniviTest 
 import unittest
 
 class testRunner(geniviTest.TestGeniviQemu):
     def test_rerecheckSystemCtl(self):
-        # check weston is running
-        self.assertTrue(self.makeTest('systemctl is-active weston', 'active\n'))
+        # check weston is running (event)
+        self.assertTrue(self.makeTest('systemctl is-active weston',
+                                      'active\n')) # Final system state and output
 
-if __name__ == '__main__':
+if __name__ == '__main__': # Expected state & output, run test
     gensuite = unittest.TestLoader().loadTestsFromTestCase(testRunner)
     unittest.TextTestRunner(verbosity=2).run(gensuite)
